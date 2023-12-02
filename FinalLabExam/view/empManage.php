3ca79e8527f16893b1473a02381ea16e97deae7c
gajorKhabo <118203937+arnabishakh@users.noreply.github.com>
2023-12-02 12:30:09 +0600

    Add files via upload

---
+++ FinalLabExam/view/empManage.php
@@ -0,0 +1,30 @@
+
+    <table border="0"  align="center">
+        <tr>
+            <td>
+                <h1>Employee Management Panel</h1>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <a href="empReg.php">Add Employee</a>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <a href="empList.php">Update Employee</a>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <a href="empDelete.php">Delete Employee</a>
+            </td>
+        </tr>
+        <tr>
+            <td  align="center">
+                <button ><a href="index.php">Home</a></button>
+                <button ><a href="index.php">Back</a></button>
+
+            </td>
+        </tr>
+    </table>
